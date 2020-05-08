8db84d870aa55d611df66ab1924d14003ff8d1b2
andres215 <andres-silva145@hotmail.es>
2020-05-07 22:18:36 -0500

    Dialogos de perfil ocupacional y de perfil profesional implementados

---
+++ ProyectoIntegrador3/src/interfaz/DialogoPerfilOcupacional.java
@@ -0,0 +1,125 @@
+package interfaz;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.border.TitledBorder;
+
+@SuppressWarnings("serial")
+public class DialogoPerfilOcupacional extends JDialog implements ActionListener {
+	public final static String GUARDAR_PERFIL = "Guardar";
+	public final static String EDITAR_PERFIL = "Editar";
+	public final static String ELIMINAR_PERFIL = "Eliminar";
+	public final static String VOLVER = "Volver";
+	
+	private JButton butGuardarPerfil, butEditarPerfil, butEliminarPerfil, butVolver;
+	private Main principal;
+	private JTextArea textPerfil;
+	private JScrollPane scroll;
+	
+	public DialogoPerfilOcupacional(Main principal) {
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		this.principal = principal;
+		setSize(new Dimension(650,480));
+		setLayout(new BorderLayout());
+		setTitle("Programa Ingeniería de Sistemas - Institución Universitaria Antonio José Camacho");
+		setResizable(false);
+		setLocationRelativeTo(null);
+				
+		butGuardarPerfil = new JButton(GUARDAR_PERFIL);
+		butGuardarPerfil.setActionCommand(GUARDAR_PERFIL);
+		butGuardarPerfil.addActionListener(this);
+		butEditarPerfil = new JButton(EDITAR_PERFIL);
+		butEditarPerfil.setActionCommand(EDITAR_PERFIL);
+		butEditarPerfil.addActionListener(this);
+		butEliminarPerfil = new JButton(ELIMINAR_PERFIL);
+		butEliminarPerfil.setActionCommand(ELIMINAR_PERFIL);
+		butEliminarPerfil.addActionListener(this);
+		butVolver = new JButton(VOLVER);
+		butVolver.setActionCommand(VOLVER);
+		butVolver.addActionListener(this);
+		
+		butGuardarPerfil.setEnabled(false);
+		
+		Font font = new Font("Verdana", Font.BOLD, 15);
+		TitledBorder border = BorderFactory.createTitledBorder("Perfil Ocupacional");
+		border.setTitleColor(Color.BLUE);
+		border.setTitleFont(font);
+		JPanel formulario = new JPanel();
+		formulario.setBorder(border);
+		formulario.setLayout(new GridLayout(1,1));
+		textPerfil = new JTextArea(principal.getPerfilOcupacional());
+		textPerfil.setLineWrap(true);
+		textPerfil.setEditable(false);
+		scroll = new JScrollPane(textPerfil);
+		formulario.add(scroll);
+		
+		JPanel opciones = new JPanel();
+		opciones.setPreferredSize(new Dimension(0,20));
+		opciones.setLayout(new GridLayout(1,3));
+		opciones.add(butGuardarPerfil);
+		opciones.add(butEditarPerfil);
+		opciones.add(butEliminarPerfil);
+		
+		JPanel aux = new JPanel();
+		aux.setLayout(new GridLayout(2,1));
+		aux.add(opciones);
+		aux.add(butVolver);
+		
+		add(formulario, BorderLayout.CENTER);		
+		add(aux, BorderLayout.SOUTH);
+		
+	}
+	
+	public void editarPerfilOcupacional() {
+		butGuardarPerfil.setEnabled(true);
+		textPerfil.setEditable(true);
+		butEditarPerfil.setEnabled(false);
+	}
+	
+	public void guardarPerfilOcupacional() {
+		principal.guardarPerfilOcupacional(textPerfil.getText());
+		textPerfil.setEditable(false);
+		butEditarPerfil.setEnabled(true);
+		butGuardarPerfil.setEnabled(false);
+	}
+	
+	public void eliminarPerfilOcupacional() {
+		textPerfil.setText("");
+		principal.guardarPerfilOcupacional("");
+	}
+	
+	public void volver() {
+		principal.cerrarDialogo(Main.VER_PERFIL_OCUPACIONAL);
+	}
+	
+	public void dispose() {
+		principal.dispose();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent evento) {
+		String comando = evento.getActionCommand();
+		if(comando.equals(EDITAR_PERFIL)) {
+			editarPerfilOcupacional();
+		}else if(comando.equals(GUARDAR_PERFIL)) {
+			guardarPerfilOcupacional();
+		}else if(comando.equals(ELIMINAR_PERFIL)) {
+			eliminarPerfilOcupacional();
+		}else {
+			volver();
+		}
+	}
+
+}
